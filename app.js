d8ca88f8ed5fb6cada0b907a27c09b4a2bf2c270
Eran Geva <eran@eran-mac.local>
2018-07-23 16:58:18 +0300

    revert canvas

--- app.js
+++ app.js
@@ -16,13 +16,13 @@ class App {
         this.renderer.gammaInput = this.renderer.gammaOutput = true;
 
         this.renderer.setSize(this.container.offsetWidth, this.container.offsetHeight);
-        this.container.appendChild(this.renderer.domElement);
+        document.body.appendChild(this.renderer.domElement);
 
         // CAMERA
         this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 3000);
         this.camera.position.set(0, -30, 50);
         this.scene.add(this.camera);
-        this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+        // this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
         // THREEx.WindowResize(this.renderer, this.camera);
 
         // Background clear color
@@ -42,13 +42,13 @@ class App {
         });
 
         this.animate = this.animate.bind(this);
-        this.animate();
+        // this.animate();
     }
 
     animate() {
         this.renderer.render(this.scene, this.camera);
         console.log(this.camera.position);
-        requestAnimationFrame(this.animate);
+        // requestAnimationFrame(this.animate);
     }
 }
 
